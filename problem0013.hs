090a222d41dba70bf4c8f8f81635af7aa5c7acec
Walther <veeti.haapsamo@gmail.com>
2013-01-18 17:40:57 +0200

    Added some documentation

--- problem0013.hs
+++ problem0013.hs
@@ -1,3 +1,4 @@
+-- Give lasgeSum a file with a list of integers separated by newlines,
+-- and it outputs the sum of the lines. Solution for Project Euler #13
+
 largeSum file = (sum . map read . lines) `fmap` readFile file :: IO Integer
-
-
