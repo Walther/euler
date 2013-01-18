d0cc1efdd7fae151c67aeb608a2c28f4dff0e9b5
Walther <veeti.haapsamo@gmail.com>
2013-01-18 17:45:17 +0200

    Added some documentation

--- problem0006.hs
+++ problem0006.hs
@@ -1,3 +1,6 @@
+-- sumSquareDiff outputs the difference between the square of the sum of all integers
+-- from 1 to n, and the sum of the squares of integers between 1 to n. Euler #6
+
 sumSquareDiff n = (sumSqu n) - (squSum n)
 	where
 		sumSqu n = (sum [1..n])^2
