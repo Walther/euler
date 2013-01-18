89a9751d068651e12fc99a9cf1e860c9e13e16a4
Walther <veeti.haapsamo@gmail.com>
2013-01-18 21:23:06 +0200

    Solved problem 15, w/ documentation

---
+++ problem0015.hs
@@ -0,0 +1,8 @@
+-- Just simply and mathematically, in n*n grid, the combinations are (2n)!/(n!*n!)
+-- gridRoutes n calculates amount of optimal paths in a n*n grid. Solution for Euler #15
+
+gridRoutes :: Integer -> Integer
+gridRoutes n = facto (2*n) `div` (facto n)^2
+	where
+		facto 1 = 1
+		facto n = n * facto (n-1)
