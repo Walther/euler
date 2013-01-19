ca6fd4a29ee0d0d8d5b6cb53afe59593646bfc58
Walther <veeti.haapsamo@gmail.com>
2013-01-19 17:57:28 +0200

    cleaned a bit

--- problem0001.hs
+++ problem0001.hs
@@ -1,4 +1,4 @@
 -- sumMultiples takes three arguments, a b c, and outputs the sum of all integers
 -- under the maximum of c that are multiples of a or b. Solution for Euler #1
 
-sumMultiples a b c = sum [x | x <- [(min a b)..(c-1)], (x `mod` a == 0) || (x `mod` b == 0)]
+sumMultiples a b c = sum [x |x <- [(min a b)..(c-1)], (x `mod` a == 0) || (x `mod` b == 0)]
