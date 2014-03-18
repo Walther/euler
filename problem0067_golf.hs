156d33a1a87e8063408be1b98247fa87c13f1787
Walther <veeti.haapsamo@gmail.com>
2014-03-18 23:43:20 +0200

    remove extra chars

--- problem0067_golf.hs
+++ problem0067_golf.hs
@@ -1,9 +1,9 @@
 r=read::String->Int
 main=do
- c <- readFile "tree.txt"
- let a=n$m (m r)$m words$filter(notElem '#')$lines c
+ c<-readFile"tree.txt"
+ let a=n$m(m r)$m words$filter(notElem '#')$lines c
  print a
 z=zipWith
 m=map
-n (xs:[])=maximum xs
-n (x:y:xs)=n$z max(z(+)x y++[0])(0:z(+)x(tail y)):xs
+n (l:[])=maximum l
+n (x:y:l)=n$z max(z(+)x y++[0])(0:z(+)x(tail y)):l
