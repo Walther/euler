6558483004c1957c799559756656f09e245a6187
Walther <veeti.haapsamo@gmail.com>
2014-03-19 23:42:26 +0200

    optimize 'tail' out

--- problem0067_golf.hs
+++ problem0067_golf.hs
@@ -1,4 +1,4 @@
 -- Golfed solution for Euler #67 in Haskell.
 -- Pipe the triangle/tree file from stdin, prints result.
--- oneliner, 141 chars without comments.
-main=interact$show.n.map(map read.words).filter(notElem '#').lines;z=zipWith;n[l]=maximum l;n(x:y:l)=n$z max(z(+)x y++[0])(0:z(+)x(tail y)):l
+-- oneliner, 136 chars without comments.
+main=interact$show.n.map(map read.words).filter(notElem '#').lines;z=zipWith;n[l]=maximum l;n(x:y:l)=n$z max(z(+)x y++[0])(z(+)(0:x)y):l
