d1653962c7e740a08679f5222ba3e4450037f21c
Walther <veeti.haapsamo@gmail.com>
2014-03-19 16:30:35 +0200

    trim map

--- problem0067_golf.hs
+++ problem0067_golf.hs
@@ -1,11 +1,10 @@
 -- Golfed solution for Euler #67 in Haskell.
 -- Pipe the triangle/tree file from stdin, prints result.
--- 8 lines, 173 chars without comments.
+-- 8 lines, 171 chars without comments.
 z=zipWith
-m=map
 n (l:[])=maximum l
 n (x:y:l)=n$z max(z(+)x y++[0])(0:z(+)x(tail y)):l
 main=do
 c<-getContents
-let a=n$m(m read.words)$filter(notElem '#')$lines c::Int
+let a=n$map(map read.words)$filter(notElem '#')$lines c::Int
 print a
