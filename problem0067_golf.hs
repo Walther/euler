69fa9c03ab074768c4476059632167c901c7443b
Walther <veeti.haapsamo@gmail.com>
2014-03-19 23:17:04 +0200

    parentheses to $

--- problem0067_golf.hs
+++ problem0067_golf.hs
@@ -1,7 +1,7 @@
 -- Golfed solution for Euler #67 in Haskell.
 -- Pipe the triangle/tree file from stdin, prints result.
--- 4 lines, 142 chars without comments.
+-- 4 lines, 141 chars without comments.
 z=zipWith
 n[l]=maximum l
 n(x:y:l)=n$z max(z(+)x y++[0])(0:z(+)x(tail y)):l
-main=interact(show.n.map(map read.words).filter(notElem '#').lines)
+main=interact$show.n.map(map read.words).filter(notElem '#').lines
