44d37556f50bd715171a0d76f2f8fb6ce8433d9e
Walther <veeti.haapsamo@gmail.com>
2014-03-19 16:57:51 +0200

    print call optimization. Yay, under 1 SMS

--- problem0067_golf.hs
+++ problem0067_golf.hs
@@ -1,10 +1,9 @@
 -- Golfed solution for Euler #67 in Haskell.
 -- Pipe the triangle/tree file from stdin, prints result.
--- 8 lines, 161 chars without comments.
+-- 8 lines, 153 chars without comments.
 z=zipWith
 n[l]=maximum l
 n(x:y:l)=n$z max(z(+)x y++[0])(0:z(+)x(tail y)):l
 main=do
 c<-getContents
-let a=n$map(map read.words)$filter(notElem '#')$lines c
-print a
+print$n$map(map read.words)$filter(notElem '#')$lines c
