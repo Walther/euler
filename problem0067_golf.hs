2785c00e81c84b91fb4908d5e8494a9ba2029e9f
Walther <veeti.haapsamo@gmail.com>
2014-03-19 16:17:35 +0200

    further whitespace trim

--- problem0067_golf.hs
+++ problem0067_golf.hs
@@ -1,11 +1,11 @@
 -- Golfed solution for Euler #67 in Haskell.
 -- Pipe the triangle/tree file from stdin, prints result.
--- 8 lines, 176 chars without comments.
-main=do
- c<-getContents
- let a=n$m(m read.words)$filter(notElem '#')$lines c::Int
- print a
+-- 8 lines, 173 chars without comments.
 z=zipWith
 m=map
 n (l:[])=maximum l
 n (x:y:l)=n$z max(z(+)x y++[0])(0:z(+)x(tail y)):l
+main=do
+c<-getContents
+let a=n$m(m read.words)$filter(notElem '#')$lines c::Int
+print a
