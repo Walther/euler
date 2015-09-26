70d29417b2c09c1f68218bc6eaf47f02145589bb
Walther <veeti.haapsamo@gmail.com>
2015-09-27 01:51:03 +0300

    improve problem0067

--- problem0067.hs
+++ problem0067.hs
@@ -4,20 +4,16 @@ import Data.List
 maxZip :: Integral a => [a] -> [a]
 maxZip xs = zipWith max xs (tail xs)
 
-adder :: Integral a => [a] -> [a] -> [a]
-adder = zipWith (+)
 
 count :: Integral a => [[a]] -> a
 count (x:[]) = head x
-count xs = count $ init (init xs) ++ [adder (last (init xs)) (maxZip (last xs))]
+count xs = count $ init (init xs) ++ [zipWith(+) (last (init xs)) (maxZip (last xs))]
 
 
 readInt = read :: String -> Int
 
 main = do
-    print "Input file: "
-    inFile <- getLine
-    content <- readFile inFile
+    content <- readFile "input/problem0067"
     let parsedLines = lines content
         uncommented = map words $ filter (notElem '#') parsedLines
         tree = map (map readInt) uncommented
@@ -26,4 +22,4 @@ main = do
     print answer
 
 -- just for testing; a pyramid of [[1],[1,2],[1,2,3],...[1..10]], answer is 55
-asdf = drop 1 $ inits [1..10]
+testpyramid = drop 1 $ inits [1..10]
