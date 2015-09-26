baa4dd9d3f47adde2e8931051500028283ffc990
Walther <veeti.haapsamo@gmail.com>
2015-09-27 02:00:11 +0300

    improve comments

--- problem0015.hs
+++ problem0015.hs
@@ -1,6 +1,12 @@
--- Just simply and mathematically, in n*n grid, the combinations are (2n)!/(n!*n!)
--- gridRoutes n calculates amount of optimal paths in a n*n grid. Solution for Euler #15
+-- Starting in the top left corner of a 2×2 grid, and only being able to move to
+-- the right and down, there are exactly 6 routes to the bottom right corner.
+-- How many such routes are there through a 20×20 grid?
 
+
+-- Just simply and mathematically, in n*n grid, the combinations are
+-- (2n)!/(n!*n!)
+
+-- gridRoutes n calculates amount of optimal paths in a n*n grid.
 gridRoutes :: Integer -> Integer
 gridRoutes n = facto (2*n) `div` (facto n)^2
     where
