5298e4a547b505fa2aa69c7561ffd69716c302e3
Walther <veeti.haapsamo@gmail.com>
2015-09-27 01:58:23 +0300

    bugfix

--- problem0014.hs
+++ problem0014.hs
@@ -12,6 +12,4 @@ chain :: Integer -> [Integer]
 chain x = (takeWhile (>1) $ iterate collatz x) ++ [1]
 
 main = do
-  print $ head $
-  maximumBy (comparing length) $
-  map chain [1..1000000]
+  print $ head $ maximumBy (comparing length) $ map chain [1..1000000]
