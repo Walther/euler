e1d4d5e009a07f53cdaab46e4fe164d79d61e1a9
Walther <veeti.haapsamo@gmail.com>
2015-09-27 00:23:51 +0300

    make the program runnable with 'runhaskell'

--- problem0001.hs
+++ problem0001.hs
@@ -1,4 +1,11 @@
--- sumMultiples takes three arguments, a b c, and outputs the sum of all integers
--- under the maximum of c that are multiples of a or b. Solution for Euler #1
+-- Find the sum of all the multiples of 3 or 5 below 1000.
 
-sumMultiples a b c = sum [x |x <- [(min a b)..(c-1)], (x `mod` a == 0) || (x `mod` b == 0)]
+main = do
+  print $ sumMultiples 3 5 1000
+
+-- sumMultiples takes three arguments a b c,
+-- returns the sum of all integers < c,
+-- that are multiples of a or b.
+
+sumMultiples a b c = sum [x | x <- [(min a b)..(c-1)],
+                         (x `mod` a == 0) || (x `mod` b == 0)]
